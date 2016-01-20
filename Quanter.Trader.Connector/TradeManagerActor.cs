9ee87a25af9c7666946791c31a982e07759cdfe6
qiujoe <qiujinglei@gmail.com>
2016-01-20 16:43:16 +0800

    完善

--- Quanter.Trader.Connector/TradeManagerActor.cs
+++ Quanter.Trader.Connector/TradeManagerActor.cs
@@ -1,4 +1,7 @@
 ﻿using Akka.Actor;
+using Akka.Event;
+using Quanter.BusinessEntity;
+using Quanter.Trader.Messages;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,18 +10,54 @@ using System.Threading.Tasks;
 
 namespace Quanter.Trader.Connector
 {
-    class TradeManagerActor : UntypedActor
+    public class TradeManagerActor : TypedActor, IHandle<TradeManagerRequest>
     {
-        protected override void OnReceive(object message)
+        private readonly ILoggingAdapter _log = Logging.GetLogger(Context);
+        private ActorSelection persistenceActor;
+        private IDictionary<int, IActorRef> traders = new Dictionary<int, IActorRef>();
+
+        public void Handle(TradeManagerRequest message)
         {
-            throw new NotImplementedException();
+            try
+            {
+                switch(message.Type)
+                {
+                    case TradeManagerRequest.RequestType.INIT:
+                        _init();
+                        break;
+                }
+            } catch (Exception e)
+            {
+                _log.Error("TradeManagerActor发生异常 {0}", e.StackTrace);
+            }
         }
 
-        private void _createTradeActor()
+        private void _init()
         {
-            // 读取数据库， exe的位置，交易软件ths|tdx
-
-            //
+            _log.Debug("从数据库中读取交易接口的信息");
+            persistenceActor = Context.ActorSelection(String.Format("/user/{0}", ConstantsHelper.AKKA_PATH_PERSISTENCE));
+            // 读取交易接口
+            PersistenceRequest req = new PersistenceRequest() { Type = PersistenceType.LIST, Body = "from ETrader" };
+            var result = persistenceActor.Ask<IList<Object>>(req, TimeSpan.FromSeconds(3));
+            result.Wait();
+            var traders = result.Result;
+            foreach(var o in traders)
+            {
+                var trader = (ETrader)o;
+                _createTradeActor(trader);
+                _log.Debug("加载交易接口 {0}", trader.Name);
+            }
         }
+
+        private void _createTradeActor(ETrader trader)
+        {
+            Type t = Type.GetType(trader.Type);
+            var tr = Context.ActorOf(Props.Create(t), trader.Id.ToString());
+
+            tr.Tell(new TradeRequest() { Type = TradeRequest.RequestType.INIT });
+
+            traders.Add(trader.Id, tr);
+        }
+
     }
 }
